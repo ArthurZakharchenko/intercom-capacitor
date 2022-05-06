9eb6db568ac1a6c39340cfd9e16dd1c73165c6b4
Zakharchenko Arthur <manclassic2@gmail.com>
2022-05-06 19:33:23 +0100

    add dist

---
+++ dist/esm/definitions.d.ts
@@ -0,0 +1,42 @@
+export interface IntercomPlugin {
+    registerIdentifiedUser(options: {
+        userId?: string;
+        email?: string;
+    }): Promise<void>;
+    registerUnidentifiedUser(): Promise<void>;
+    updateUser(options: IntercomUserUpdateOptions): Promise<void>;
+    logout(): Promise<void>;
+    logEvent(options: {
+        name: string;
+        data?: any;
+    }): Promise<void>;
+    displayMessenger(): Promise<void>;
+    displayMessageComposer(options: {
+        message: string;
+    }): Promise<void>;
+    displayHelpCenter(): Promise<void>;
+    hideMessenger(): Promise<void>;
+    displayLauncher(): Promise<void>;
+    hideLauncher(): Promise<void>;
+    displayInAppMessages(): Promise<void>;
+    hideInAppMessages(): Promise<void>;
+    displayCarousel(options: {
+        carouselId: string;
+    }): Promise<void>;
+    setUserHash(options: {
+        hmac: string;
+    }): Promise<void>;
+    setBottomPadding(options: {
+        value: string;
+    }): Promise<void>;
+}
+export interface IntercomUserUpdateOptions {
+    userId?: string;
+    email?: string;
+    name?: string;
+    phone?: string;
+    languageOverride?: string;
+    customAttributes: {
+        [key: string]: any;
+    };
+}
